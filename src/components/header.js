e24b145a412926baed79d6633fad9f7ec5336aec
Timothy Stanislav <tmstani23@gmail.com>
2021-11-30 15:35:57 -0600

    props 1
    added news json.  Added news props to the newslist component and rendered some of the json items.

--- src/components/header.js
+++ src/components/header.js
@@ -31,38 +31,17 @@ import React, {Component} from 'react';
 class Header extends Component {
     
     state = {
-        name: 'Tim',
+        
         keywords: '',
-        title: 'The keywords are: ',
-        count: 0
-    }
-
-    hello () {
-        console.log('hello world');
     }
 
     inputChangeHandler = (event) => {
-        //console.log(event.target.value)
-        // this.hello();
+        
         this.setState({
             keywords: event.target.value
         })
     }
-    
-    // inputWithParamsHandler(event, name) {
-    //     console.log(event.target.value)
-    //     console.log(name + event.target.value)
-    // }
 
-    addOne() {
-        //can do this way or with a callback
-        // this.setState({count: this.state.count + 1})
-        
-        //with callback get access to prevState and props
-        this.setState(( state, props) => ({
-            count: state.count + 1
-        }))
-    }
     
     render() {
         return(
@@ -74,11 +53,6 @@ class Header extends Component {
                 <input onChange={this.inputChangeHandler} type="text" />
                 {/* to pass in multiple params must use an encapsulating function and pass in the event */}
                 {/* <input onChange={(event) => this.inputWithParamsHandler(event, "Artul")} type="text" /> */}
-                <div>{this.state.title}</div>
-                <div>{this.state.keywords}</div>
-                <br />
-                <div>{this.state.count}</div>
-                <button onClick={() => this.addOne()}>Add One</button>
             </header>
             
         )
--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import Header from "./components/header";
 import './styles-css/style.css';
+import dbJSON from './styles-css/db.json'
 
-const App = () => (
-    <>
-        <Header />
-    </>
-)
+import Header from "./components/header";
+import NewsList from 
+"./components/news_list.js"
+
+class App extends Component {
+    
+    state = {
+        news: dbJSON
+    }
+
+    render() {
+        console.log(this.state.news)
+        return (
+            <>
+                <Header />
+                <NewsList 
+                    news={this.state.news}
+                />
+            </>
+        )
+        
+    }
+    
+}
     
 //first param = component and second = where it goes
 ReactDOM.render(<App/>, document.getElementById('root'));
