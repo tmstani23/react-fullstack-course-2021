4c4bfdfdb2be8917b10ac7f82e67b9deee04a942
Timothy Stanislav <tmstani23@gmail.com>
2021-12-09 05:33:51 -0600

    useState Update1
    Added a component that includes:
    multi property useState,
    add and subtract count using useState,
    a list of posts,
    add a new post on button click using use/setState

--- 02-hooks/src/App.js
+++ 02-hooks/src/App.js
@@ -3,21 +3,66 @@ import React, {useState} from 'react';
 
 const App = ({initialCount}) => {
 
-  
-  let [count, setCount] = useState(initialCount);
+  let [state, setState] = useState({
+    count: initialCount,
+    user: "Timothy"
+  });
+
+  let [posts, setPosts] = useState([
+    {
+      name: 'Super awesome post',
+      body: 'The content of the post'
+    },
+    {
+      name: 'titanium',
+      body: `shoot me down but I won't fall.  I am titanium`
+    },
+  ])
 
   const subtractOne = () => {
-    setCount(prevCount => {
-        return prevCount - 1;
+    setState({
+      ...state,
+      count: state.count - 1
     });
+  }
+
+  const addPosts = () => {
+    let newPost = {
+      name: 'Legend',
+      body: `When I die I want to be a legend.`
+    }
+    return setPosts([
+      ...posts,
+      newPost
+    ])
   }
 
   return (
     <>
-      <h3>Count: {count}</h3>
-      <button onClick={() => {setCount(count + 1)}}>Add one</button>
+      <h1>{state.user}</h1>
+      <h3>Count: {state.count}</h3>
+      <button onClick={() => setState({
+        ...state,
+        count: state.count + 1
+      })}>Add one</button>
       <button onClick={subtractOne}>Subtract one</button>
-      <button onClick={() => setCount(initialCount)}>Reset count</button>
+      {/* <button onClick={() => setCount(initialCount)}>Reset count</button> */}
+
+      <hr />
+
+      {
+        posts.map((item, i) => (
+          <div key = {i}>
+            <h1>Name: {item.name}</h1>
+            <h3>Body: {item.body}</h3>
+          </div>
+        ))
+      }
+
+      <button onClick={addPosts}>
+        Add new post
+      </button>
+
     </>
   );
 }
